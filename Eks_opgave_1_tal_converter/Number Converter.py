47d4f0ad0dcd7470969320e3011824316b5a0ef9
MrCowsen <58846830+MrCowsen@users.noreply.github.com>
2021-10-24 18:51:39 +0200

    Added comments
    Added comments to all functions
    Only discription and group aproval left

--- Eks_opgave_1_tal_converter/Number Converter.py
+++ Eks_opgave_1_tal_converter/Number Converter.py
@@ -1,62 +1,74 @@
 
 """
 Created on Wed Oct 20 13:20:32 2021
+
 @author: @authors: Mads Andersen, Eric van den Brand, Daniel Hansen, Thor Skatka og Andreas Hansen
+
+Beskrivelse: 
 """
 
 
 def format_number(number):
+    # Formates number with , for added readability
     ready_number = '{:,}'.format(number)
+    
     return ready_number
 
 
 # ! int_to_bin to convert integer to binary
-# Create an empty list, then take the entered integer and modulus by 2
-# Add the modulus to the list, then use floor division to redefine the entered number
-# Perform until i < 0. Reverse the list, print the list
 def int_to_bin(x):
+    # Create an empty string, then take the entered integer and modulus by 2
     a = ""
+    
+    # Add the modulus to the string, then use floor division to redefine the entered number
+    # Perform until x = 0. 
     while x > 0:
         n = x % 2
         a += str(n)
         x = x // 2
-    z = a[::-1] #Reverse the string
+    
+    z = a[::-1] # Reverse the string
+    
     return z
         
 
 # ! par_int_rom and int_to_rom convert integers to roman numerals
 def par_int_rom(x):
-        rom_num = ["M" , "CM" , "D" , "CD" , "C" , "XC" , "L" , "XL" , "X" , "IX" , "V" ,"IV", "I" ]
-        integers = [1000 , 900 , 500 , 400 , 100 , 90 , 50 , 40 , 10 , 9 , 5 , 4 , 1]
-        
-        i = 0
-        rom_str = ""
-        
-        while x > 0:
-            while (x // integers[i]) > 0:
-                rom_str += rom_num[i]
-                x -= integers[i]
-            i+=1
-        
-        return rom_str
+    rom_num = ["M" , "CM" , "D" , "CD" , "C" , "XC" , "L" , "XL" , "X" , "IX" , "V" ,"IV", "I" ]
+    integers = [1000 , 900 , 500 , 400 , 100 , 90 , 50 , 40 , 10 , 9 , 5 , 4 , 1]
+    
+    i = 0
+    rom_str = "" # Creates en empty string
+    
+    # If x // integers[i] > 0, it adds the roman numeral to rom_str and subtracts integers[i] form x
+    # When x // inegers[i] > 0  not anymore, then it repeats for integers[i+1], until end of integers
+    while x > 0:
+        while (x // integers[i]) > 0:
+            rom_str += rom_num[i]
+            x -= integers[i]
+        i+=1
+    
+    return rom_str # It then returns rom_str
 
-
+ 
 def int_to_rom(x):
-    if x >= 4000:
-        big = x // 1000
-        big_roman = par_int_rom(big)
+    if x >= 4000: # If x >= 4000, it splits x in two parts:
+        big = x // 1000 # Big part, wich are the roman numerals that need to be multiplied by 1000
+        tiny = x - big * 1000 # And tiny part, wich is whats left of x after big is subtracted
         
-        tiny = x - big * 1000
-        tiny_roman = par_int_rom(tiny)
+        # big_rom and tiny_rom are then made by par_int_rom
+        big_rom = par_int_rom(big)
+        tiny_rom = par_int_rom(tiny)
         
-        roof = '_' * len(big_roman)
-        print_ready_roman = f"{big_roman}{tiny_roman}"
+        roof = '_' * len(big_rom) # roof is then made in length of big_rom
+        print_ready_rom = f"{big_rom}{tiny_rom}" # big_rom and tiny_rom are added together
         
-        print_it_all = f'''{roof}\n{print_ready_roman}'''
+        # And the roman numeral gets printed as one big multiline string
+        print_it_all = f'''{roof}\n{print_ready_rom}'''
         
         return print_it_all
         
-    else:
+    else: # If x < 4000 it just returns par_int_rom(x)
         return par_int_rom(x)
     
 
@@ -77,8 +89,13 @@ def bin_to_int(x):
 def par_rom_int(x):
     rom_num = ["M" , "CM" , "D" , "CD" , "C" , "XC" , "L" , "XL" , "X" , "IX" , "V" ,"IV", "I" ]
     integers = [1000 , 900 , 500 , 400 , 100 , 90 , 50 , 40 , 10 , 9 , 5 , 4 , 1]
+    
     value = 0
     
+    # Goes through rom_num list, if first part of x == rom_num[i]: value += integers[i]
+    # Delete first part it found a value for, and repeats
+    # When it doesnt find a match for rom_num[i], 
+    # it goes to rom_num[i+1] and repeats until end of rom_num
     for i in range(len(rom_num)):
         while x[:len(rom_num[i])] ==  rom_num[i]:
             value += integers[i]
@@ -89,16 +106,19 @@ def par_rom_int(x):
 
 def rom_to_int(x):
     if ' ' in x:
+        # Splits string into big_rom and tiny_rom
         big_rom,tiny_rom = [str(i) for i in x.split(' ')]
         
+        # Runs par_rom_int for big_rom and tiny_rom
         big_int = par_rom_int(big_rom)
         tiny_int = par_rom_int(tiny_rom)
         
+        # Adds big_int * 1000 and tiny_int together
         number = (big_int * 1000) + tiny_int
         
         return format_number(number)
     
-    else:
+    else: # If x has no ' ', run par_rom_int and return result
         number = par_rom_int(x)
         return format_number(number)
 
@@ -113,6 +133,10 @@ def menu():
         #3. Roman
         #4. Terminate Program
         --->: ''')
+        
+        if from_sys == '4': # Terminates program
+            print('You terminated the program')
+            return
         
         user_number = input('''Please enter your number (if its a roman numeral of 4000 
         and above, please type a space after the part that needs to be multiple by 1000): 
@@ -131,27 +155,24 @@ def menu():
         elif from_sys == '1' and to_sys == '3': # int to rom
             print(int_to_rom(int(user_number)))
             
-        elif from_sys == '2' and to_sys == '1': #bin to int
+        elif from_sys == '2' and to_sys == '1': # bin to int
             print(bin_to_int(user_number))
             
-        elif from_sys == '3' and to_sys == '1': #rom to int
+        elif from_sys == '3' and to_sys == '1': # rom to int
             print(rom_to_int(user_number))
             
         elif from_sys == '2' and to_sys == '3': # bin to rom
             bin_int = bin_to_int(user_number)
-            print(int_to_rom(bin_int)) # rom to bin
+            print(int_to_rom(bin_int)) # int to rom
             
-        elif from_sys == '3' and to_sys == '2':
+        elif from_sys == '3' and to_sys == '2': # rom to bin
             rom_int = rom_to_int(user_number)
-            print(int_to_bin(int(rom_int)))
+            print(int_to_bin(int(rom_int))) # int to bin
             
         # User tries to convert from and to the same numerical system
         elif from_sys == to_sys:
             print('You are trying to convert one numerical system to the same. Please try again')
             menu()
-        
-        elif from_sys == '4': # Terminates program
-            return
             
         else: # Loops for invalid input
             print('Invalid input.')
